00e783f119dc95813d6bd5c1de51b8f528521399
atusy <30277794+atusy@users.noreply.github.com>
2025-04-23 21:17:43 +0900

    fix(jab): update typing

--- lua/jab/_type.lua
+++ lua/jab/_type.lua
@@ -3,6 +3,11 @@
 ---@field str string
 ---@field label string
 ---@field instant boolean
+---@field win number
+---@field buf number
+
+---@class JabSearchOpts
+---@field regex string
 
 ---@class JabMatch
 ---@field row number
@@ -13,5 +18,5 @@
 ---@field width_label number?
 
 ---@alias JabKind "f" | "F" | "t" | "T" | "window"
----@alias JabFun fun(kind: JabKind, labels: string[]?, opts: JabOpts?): string?
+---@alias JabFun fun(kind: JabKind, labels: string[]?, opts: JabOpts | JabSearchOpts | nil): string?
 ---@alias JabMotionFun fun(labels: string[]?): string?
